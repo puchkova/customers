4b1ba7780007dd14cb32c9b7a434c2440a7a7134
tatjana.putskova <tatjana.putskova@ttu.ee>
2021-08-12 17:38:06 +0300

    fill database

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -1,13 +1,47 @@
 CREATE TABLE customers
 (
-    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    id        uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     firstname text COLLATE pg_catalog."default",
-    lastname text  COLLATE pg_catalog."default",
-    birthdate date ,
-    gender text COLLATE pg_catalog."default",
-    email text COLLATE pg_catalog."default",
-    address text COLLATE pg_catalog."default"
+    lastname  text COLLATE pg_catalog."default",
+    birthdate date,
+    gender    text COLLATE pg_catalog."default",
+    email     text COLLATE pg_catalog."default",
+    address   text COLLATE pg_catalog."default"
 );
 
-INSERT INTO customers(id, firstname, lastname, birthdate, gender, email, address) VALUES
-    (gen_random_uuid(), 'Tatjana', 'Putskova', '1993-05-21', 'Female', 'email@gmail.com', 'Sütiste tee 19, Tallinn' );
+INSERT INTO customers(id, firstname, lastname, birthdate, gender, email, address)
+VALUES (gen_random_uuid(), 'Tatjana', 'Putskova', '1993-05-21', 'Female', 'email@gmail.com',
+        '19 Sütiste tee, Tallinn, Estonia'),
+       (gen_random_uuid(), 'Tatjana', 'Larina', '2001-05-21', 'Female', 'email@gmail.com',
+        'Pskovskaja gubernja, Russian Empire'),
+       (gen_random_uuid(), 'Homer ', 'Simpson', '1956-03-12', 'Male', 'email@gmail.com',
+        '742 Evergreen Terrace, Springfield, United States'),
+       (gen_random_uuid(), 'Bart  ', 'Simpson', '1981-02-23', 'Male', 'email@gmail.com',
+        '742 Evergreen Terrace, Springfield'),
+       (gen_random_uuid(), 'Lisa  ', 'Simpson', '1981-05-09', 'Female', 'email@gmail.com',
+        '742 Evergreen Terrace, Springfield'),
+       (gen_random_uuid(), 'Maggie  ', 'Simpson', '1987-04-19', 'Female', 'email@gmail.com',
+        '742 Evergreen Terrace, Springfield'),
+       (gen_random_uuid(), 'Abraham  ', 'Simpson II', '2003-08-10', 'Male', 'email@gmail.com',
+        '742 Evergreen Terrace, Springfield'),
+       (gen_random_uuid(), 'Ned  ', 'Flanders', '1962-05-21', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Charles', 'Montgomery Burns', '1963-11-22', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Krusty ', 'Clown', '1963-11-22', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Seymour', 'Skinner', '1963-11-22', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Milhouse ', 'Houten', '1963-11-22', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Edna  ', 'Krabappel', '1963-11-22', 'Female', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Apu  ', 'Nahasapeemapetilon', '1993-05-21', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Eleanor   ', 'Abernathy', '1962-05-21', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Akira', 'Akira', '1963-11-22', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Ms. Albright ', 'Albright', '1963-11-22', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Woody ', 'Skinner', '1963-11-22', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Tattoo  ', 'Annie', '1963-11-22', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Aristotle  ', 'Amadopolis', '1963-11-22', 'Female', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'State ', 'Comptroller Atkins', '1993-05-21', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Mary ', 'Bailey', '1962-05-21', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Jasper ', 'Beardly', '1963-11-22', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Benjamin  ', 'nerd', '1963-11-22', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Bill ', 'KBBL DJ', '1963-11-22', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Billy ', 'actor', '1963-11-22', 'Male', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Birch ', 'Barlow', '1963-11-22', 'Female', 'email@gmail.com', ''),
+       (gen_random_uuid(), 'Black ', 'Weasel', '1993-05-21', 'Male', 'email@gmail.com', '');
